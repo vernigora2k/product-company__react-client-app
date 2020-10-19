ccecfc97a061eb3e1ff86d2edcb14820e513eefd
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2020-10-19 13:09:21 +0300

    v1.0.0stable added clean template react_redux_saga

---
+++ product-company__client-react/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import {  } from './Redux/actions' 
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <main className="App__main">
+
+      </main>
+      <footer className="App__footer">
+        
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ product-company__client-react/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { createStore, compose, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { rootReducer } from '../src/Redux/rootReducer'
+import createSagaMiddleware from 'redux-saga'
+import { sagaAllItemWatcher, sagaIdItemWatcher } from './Redux/sagas'
+
+const saga = createSagaMiddleware()
+
+const store = createStore(rootReducer, compose(
+  applyMiddleware(saga),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+))
+
+saga.run(sagaAllItemWatcher)
+saga.run(sagaIdItemWatcher)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
